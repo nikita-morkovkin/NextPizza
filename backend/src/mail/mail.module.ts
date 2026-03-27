9866e9f0ab1548131772285ae3c02491c70b8be2
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2026-03-27 13:21:11 +0500

    refactor(mail): import required modules in mail module
    Add PrismaModule and ConfigModule imports to enable database access and configuration management within the mail service.

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { MailService } from './mail.service';
 
 @Module({
+  imports: [PrismaModule, ConfigModule],
   providers: [MailService],
   exports: [MailService],
 })
